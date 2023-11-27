0a27541027e16b3b8635644000f5021188b42f13
Juan Sebastian Castañeda Burbano <sicastaprojectos@gmail.com>
2023-11-27 14:33:08 -0500

    adding cors

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import json
 import os
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 from data.gastronomic_industria_dataframe import restaurants_json
 
 app = Flask(__name__)
 
+CORS(app)
 
+
+@cross_origin
 @app.route("/restaurants")
 def get_restaurants():
     return {"restaurants": json.loads(restaurants_json)}
