dd7dcf65b8b8f51238a98e90d37a463657665de9
Juan Sebastian Castañeda Burbano <sicastaprojectos@gmail.com>
2023-11-27 13:05:46 -0500

    pq2

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import json
 import os
 from flask import Flask
 
-# from data.gastronomic_industria_dataframe import restaurants_json
+from data.gastronomic_industria_dataframe import restaurants_json
 
 app = Flask(__name__)
 
 
 @app.route("/restaurants")
 def get_restaurants():
-    # return {"restaurants": json.loads(restaurants_json)}
-    return "a"
+    return {"restaurants": json.loads(restaurants_json)}
 
 
 if __name__ == "__main__":
